455893a9cb95785594b53d1814f582bc9cfc3864
tpierrain <thomas@pierrain.net>
2021-01-02 00:45:06 +0100

    Externalize Latin words in a dedicated type.

--- Diverse/Strings/LoremFuzzer.cs
+++ Diverse/Strings/LoremFuzzer.cs
@@ -11,44 +11,6 @@ namespace Diverse.Strings
     {
         private readonly IFuzz _fuzzer;
 
-        private static readonly string[] _words = new[] { "alias", "consequatur", "aut", "perferendis", "sit", "voluptatem",
-        "accusantium", "doloremque", "aperiam", "eaque","ipsa", "quae", "ab",
-        "illo", "inventore", "veritatis", "et", "quasi", "architecto",
-        "beatae", "vitae", "dicta", "sunt", "explicabo", "aspernatur", "aut",
-        "odit", "aut", "fugit", "sed", "quia", "consequuntur", "magni",
-        "dolores", "eos", "qui", "ratione", "voluptatem", "sequi", "nesciunt",
-        "neque", "dolorem", "ipsum", "quia", "dolor", "sit", "amet",
-        "consectetur", "adipisci", "velit", "sed", "quia", "non", "numquam",
-        "eius", "modi", "tempora", "incidunt", "ut", "labore", "et", "dolore",
-        "magnam", "aliquam", "quaerat", "voluptatem", "ut", "enim", "ad",
-        "minima", "veniam", "quis", "nostrum", "exercitationem", "ullam",
-        "corporis", "nemo", "enim", "ipsam", "voluptatem", "quia", "voluptas",
-        "sit", "suscipit", "laboriosam", "nisi", "ut", "aliquid", "ex", "ea",
-        "commodi", "consequatur", "quis", "autem", "vel", "eum", "iure",
-        "reprehenderit", "qui", "in", "ea", "voluptate", "velit", "esse",
-        "quam", "nihil", "molestiae", "et", "iusto", "odio", "dignissimos",
-        "ducimus", "qui", "blanditiis", "praesentium", "laudantium", "totam",
-        "rem", "voluptatum", "deleniti", "atque", "corrupti", "quos",
-        "dolores", "et", "quas", "molestias", "excepturi", "sint",
-        "occaecati", "cupiditate", "non", "provident", "sed", "ut",
-        "perspiciatis", "unde", "omnis", "iste", "natus", "error",
-        "similique", "sunt", "in", "culpa", "qui", "officia", "deserunt",
-        "mollitia", "animi", "id", "est", "laborum", "et", "dolorum", "fuga",
-        "et", "harum", "quidem", "rerum", "facilis", "est", "et", "expedita",
-        "distinctio", "nam", "libero", "tempore", "cum", "soluta", "nobis",
-        "est", "eligendi", "optio", "cumque", "nihil", "impedit", "quo",
-        "porro", "quisquam", "est", "qui", "minus", "id", "quod", "maxime",
-        "placeat", "facere", "possimus", "omnis", "voluptas", "assumenda",
-        "est", "omnis", "dolor", "repellendus", "temporibus", "autem",
-        "quibusdam", "et", "aut", "consequatur", "vel", "illum", "qui",
-        "dolorem", "eum", "fugiat", "quo", "voluptas", "nulla", "pariatur",
-        "at", "vero", "eos", "et", "accusamus", "officiis", "debitis", "aut",
-        "rerum", "necessitatibus", "saepe", "eveniet", "ut", "et",
-        "voluptates", "repudiandae", "sint", "et", "molestiae", "non",
-        "recusandae", "itaque", "earum", "rerum", "hic", "tenetur", "a",
-        "sapiente", "delectus", "ut", "aut", "reiciendis", "voluptatibus",
-        "maiores", "doloribus", "asperiores", "repellat"};
-
         private static int MinNumberOfWordsInASentence = 2;
 
         /// <summary>
@@ -72,7 +34,7 @@ namespace Diverse.Strings
             var result = new List<string>();
             for (var i = 0; i < number.Value; i++)
             {
-                var word = _fuzzer.PickOneFrom(_words);
+                var word = _fuzzer.PickOneFrom(Latin.Words);
                 result.Add(word);
             }
 
